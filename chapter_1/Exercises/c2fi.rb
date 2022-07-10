5b49dad8c5d80a4d99e8a1399dc83f1186aa398b
irendon1993 <isaacrendon7@gmail.com>
2022-07-10 12:18:16 -0400

    celsius to fahrenheit

---
+++ chapter_1/Exercises/c2fi.rb
@@ -0,0 +1,8 @@
+print "Hello. Enter a Celsius value: "
+# celsius = gets
+# fahrenheit = (celsius.to_i * 9  / 5) +32 
+# print "The Fahrenheit equivalent is "
+# print fahrenheit
+# puts "."
+print "The Fahrenheit equivalent is ",
+ gets.to_i * 9 / 5+ 32, ".\n"
